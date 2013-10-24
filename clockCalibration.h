1a6723a88496a8e248d5c528c0084c7eefe163cf
Ryan Hub <c15ryan.hub@usafa.edu>
2013-10-24 13:18:52 -0600

    added the header file for the Clock Calibration repo

---
+++ clockCalibration.h
@@ -0,0 +1,16 @@
+
+
+#include <msp430.h>
+#ifndef CLOCKCALIBRATION_H_
+#define CLOCKCALIBRATION_H_
+
+void clock_1MHZ();
+
+void clock_8MHZ();
+
+void clock_12MHZ();
+
+void clock_16MHZ();
+
+
+#endif /* CLOCKCALIBRATION_H_ */
